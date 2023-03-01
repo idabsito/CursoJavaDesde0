e9e14fc1c3b0b42eedf11d3f8a186e34c644abe1
idabsito <sirklassen@gmail.com>
2023-03-01 10:11:39 -0300

    Ejemplos de Variables y objetos sencillos

---
+++ src/main/java/com/Enki/sesion01/ObjetosSencillos.java
@@ -0,0 +1,77 @@
+package com.Enki.sesion01;
+
+public class ObjetosSencillos {
+    public static void main(String[] args) {
+        //Objetos Sencillos en Java
+
+            //Objeto booleanos
+                //Declaramos el objeto
+                Boolean activo;
+                //Inicializando
+                activo = true;
+                System.out.println(activo);
+                //cambiamos el valor
+                activo= false;
+                System.out.println(activo);
+
+            //Caracteres
+                //Declarando e Inicializacion de una cadena tipo String
+                String cadena = "Hola Chat";
+                System.out.println(cadena);
+                //Declarando e Inicializacion de un objeto en base a una funcion constructora
+                String cadena1 = new String("Nuevo Valor");
+                System.out.println(cadena1);
+                // Asignamos otro valor a la referencia del objeto
+                cadena1 = "otro valor";
+                //El objeto que contenia "nuevo valor" se ha "PERDIDO"
+                // EL recolector de basura liberara en algun momento esa memoria ocupada
+                System.out.println(cadena1);
+                // Acceder a propiedades del objeto
+                System.out.println(cadena1.length());
+
+                //Objetos de numeros
+                //Numeros enteros
+                Integer numeroEntero = 12;
+                System.out.println(numeroEntero);
+                //Numero flotante
+                Float flotante = 12.3F;
+                System.out.println(flotante);
+                //Numeros double
+                Double doble = 12.3434343;
+                System.out.println(doble);
+
+                //Apocopes
+                int i = 1;
+                System.out.println(i);
+                i=i+1;
+                System.out.println(i);
+                //suma
+                i+=2;
+                System.out.println(i);
+                //resta
+                i-=2;
+                System.out.println(i);
+                //multiplicacion
+                i*=3;
+                System.out.println(i);
+                System.out.println("-------------------------------");
+
+                //incrementos y decrementos
+                i=3;
+                //pre incremento
+                ++i;
+                System.out.println(i);
+                //post incremento
+                i++;
+                System.out.println(i);
+                //pre decremento
+                --i;
+                System.out.println(i);
+                //post decremento
+                i--;
+                System.out.println(i);
+
+
+
+    }
+}
--- src/main/java/com/Enki/sesion01/Variables.java
+++ src/main/java/com/Enki/sesion01/Variables.java
@@ -1,4 +1,4 @@
-package com.Enki;
+package com.Enki.sesion01;
 
 public class Variables {
     public static void main(String[] args) {
